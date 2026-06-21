629b4cdbfb70ac82494613ef7d0483db2951af45
sergejgrigorev2861-sys <sergejgrigorev2861@gmail.com>
2026-06-21 13:17:33 +0300

    feat: add classes Product and Category

---
+++ src/classes.py
@@ -0,0 +1,19 @@
+class Product:
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.category_count += 1
+        Category.product_count += len(products)
